daec489f56163d6c002d9856bca8fb45043fc2bb
Korges <korgesik@gmail.com>
2019-10-27 18:17:32 +0100

    Mission 3: Ready diagrams

---
+++ src/app/space/fighter-ship.ts
@@ -0,0 +1,7 @@
+import { SpaceShip } from './space-ship';
+
+export class FighterShip extends SpaceShip {
+  constructor() {
+    super('Me 262', '/assets/me-262.png');
+  }
+}
---
+++ src/app/space/jet-bomber-ship.ts
@@ -0,0 +1,7 @@
+import { SpaceShip } from './space-ship';
+
+export class JetBomberShip extends SpaceShip {
+  constructor() {
+    super('Arado', '/assets/Ar-234.png');
+  }
+}
