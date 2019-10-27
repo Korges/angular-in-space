32b50f44ed6b81f8ac37a14834331d78569b0517
Korges <korgesik@gmail.com>
2019-10-27 20:44:02 +0100

    Mission 6: Combat assignment of pilots

--- src/app/space/hangar/hangar.component.ts
+++ src/app/space/hangar/hangar.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SpaceShip } from '../space-ship';
 import { FighterShip } from '../fighter-ship';
 import { JetBomberShip } from '../jet-bomber-ship';
 import { Pilot } from '../pilot';
+import { PilotRoomComponent } from '../pilot-room/pilot-room.component';
 
 @Component({
   selector: 'app-hangar',
@@ -10,14 +11,25 @@ import { Pilot } from '../pilot';
   styleUrls: ['./hangar.component.css']
 })
 export class HangarComponent implements OnInit {
-
+  @ViewChild(PilotRoomComponent, {static: false}) pilotRoom: PilotRoomComponent;
   spaceShips: SpaceShip[] = [];
+  selectedPilot: Pilot = null;
 
   constructor() { }
 
   ngOnInit() {
       this.spaceShips.push(new FighterShip());
       this.spaceShips.push(new JetBomberShip());
+  }
+
+  assignPilot(spaceShip: SpaceShip): void {
+    spaceShip.pilot = this.selectedPilot;
+    this.pilotRoom.pilotLeave();
+  }
+
+  deassignPilot(spaceShip: SpaceShip): void {
+    this.pilotRoom.pilotReturn(spaceShip.pilot);
+    spaceShip.pilot = null;
   }
 
 }
--- src/app/space/pilot-room/pilot-room.component.ts
+++ src/app/space/pilot-room/pilot-room.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Pilot } from '../pilot';
 
 @Component({
@@ -7,6 +7,7 @@ import { Pilot } from '../pilot';
   styleUrls: ['./pilot-room.component.css']
 })
 export class PilotRoomComponent implements OnInit {
+  @Output() selected = new EventEmitter<Pilot>();
   pilots: Pilot[] = [];
   selectedPilot: Pilot = null;
 
@@ -17,6 +18,17 @@ export class PilotRoomComponent implements OnInit {
 
   select(pilot: Pilot): void {
     this.selectedPilot = pilot;
+    this.selected.emit(pilot);
+  }
+
+  pilotLeave(): void {
+    const index = this.pilots.indexOf(this.selectedPilot);
+    this.pilots.splice(index, 1);
+    this.select(null);
+  }
+
+  pilotReturn(pilot: Pilot): void {
+    this.pilots.push(pilot);
   }
 
 }
