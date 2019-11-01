bb735c975b4dd8ce2655f9196aa6773d41fcc127
Korges <korgesik@gmail.com>
2019-11-01 21:03:56 +0100

    Mission 12: Black hole detector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpaceModule } from './space/space.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BlackHoleComponent } from './black-hole/black-hole.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlackHoleComponent
   ],
   imports: [
     BrowserModule,
